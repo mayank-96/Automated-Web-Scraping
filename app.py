5915245b5ca98c7c4b3e0dd821fb4bd3a3a65f34
mayank-96 <pagarmayank07@gmail.com>
2021-05-02 13:24:43 +0530

    Testing: 1:30 PM

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def scheduler():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     #Trigger scraper function everyday on 12 AM
-    if current_time == "14:10:00":   
+    if current_time == "13:30:00":   
         scraper()
 
 
